75b21b879e1e39e2551b4c98d67bd33489ad2e96
xR3KyZx <48029764+xR3KyZx@users.noreply.github.com>
2023-04-19 15:02:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    $cookie = "laravel_session=eyJpdiI6ImtDZk9teU9JRDg3QU9nXC82eW40cmhRPT0iLCJ2YWx1ZSI6IkI0MlV1aFk3VFVuNEp0c2FwSDB6MmZIY2dkaWlcL0Q5UzdqQ3l4VnI0ZjJRUWk2K05hcGNwT0U4QXB1ajI0MkNTIiwibWFjIjoiYWZjYWZmYWY0MDEyYzUzNTM4ZGJhMDk0ZGJjYWM0YWEyY2UwZjFiNTg1NGQ2MThkYWZjNWJlNWY3NDM0NTkwYyJ9";
+    $cookie = "XSRF-TOKEN=eyJpdiI6IlVlOENDNHg1ellvaE1vVkkxTk9cL1Z3PT0iLCJ2YWx1ZSI6IkxLdU1jWVhqbFFud2QrbENqSmN6SHVHNUtGazBlU3NHZUwxbHdaWkhiUDJvQ1NBWFd4UWpjMWNFZUFNRklPckkiLCJtYWMiOiI2Yzk3NzZmMWI1N2VlZWQ0ZTE2NzliNWZmOWNkNzEwZTMxYTAyOWU1ZTE5N2M0YzAxOWM0MzA1MzgzOGU5NTkwIn0%3D; laravel_session=eyJpdiI6ImtDZk9teU9JRDg3QU9nXC82eW40cmhRPT0iLCJ2YWx1ZSI6IkI0MlV1aFk3VFVuNEp0c2FwSDB6MmZIY2dkaWlcL0Q5UzdqQ3l4VnI0ZjJRUWk2K05hcGNwT0U4QXB1ajI0MkNTIiwibWFjIjoiYWZjYWZmYWY0MDEyYzUzNTM4ZGJhMDk0ZGJjYWM0YWEyY2UwZjFiNTg1NGQ2MThkYWZjNWJlNWY3NDM0NTkwYyJ9; AWSALB=LnoF5u/H5qpT0bkgWLB7VFNxhLuP+M7RXrCkcvqCjCcxBggDOvfR1GHg3g+SgJ0zzotNaepIi2wO86vczjLUHjHIoMCCK3cD/uzilkQ4j8dgl9HxvxdYLNZ9mp2G; AWSALBCORS=LnoF5u/H5qpT0bkgWLB7VFNxhLuP+M7RXrCkcvqCjCcxBggDOvfR1GHg3g+SgJ0zzotNaepIi2wO86vczjLUHjHIoMCCK3cD/uzilkQ4j8dgl9HxvxdYLNZ9mp2G";
 
     $headers = array(
         "Content-Type: application/x-www-form-urlencoded",
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cambio NIR Bait</title>
+        <title>Cambio de NIR Bait</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     </head>
     <style>
@@ -75,10 +75,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         <div class="container" style="text-align: center;">
             <form method="get" id="myForm">
                 <div>
-                    <input type="text" name="DN" id="textbox1" value="5500000000" placeholder="value">
+                    <input type="text" name="value" id="textbox1" value="5500000000" placeholder="DN">
                 </div>
                 <div>
-                    <input type="text" name="nir" id="textbox2" value="123" placeholder="nir">
+                    <input type="text" name="nir" id="textbox2" value="123" placeholder="NIR">
                     <input type="hidden" name="msisdnType" value="1">
                 </div>
                 <div>
